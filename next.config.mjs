2ee2b72b2f7388da5f9c4d86947fb5c049d91b7d
Shreyash <shreyash3087@gmail.com>
2025-07-06 00:18:05 +0530

    fix: Cross-Origin-Opener-Policy policy would block the window

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
